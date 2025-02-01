71ceae8fe22e5ff4bfb3dcfec218d0156f7f8263
Ayush Sahu <sahuayush0613@gmail.com>
2025-02-02 03:16:29 +0530

    add multer and cloudinary files for file upload

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -10,7 +10,7 @@ const userSchema = new Schema(
             unique:true,
             lowercase:true,
             trim:true,
-            index:true
+            index:true 
         },
         email:{
             type:String,
@@ -19,7 +19,7 @@ const userSchema = new Schema(
             lowercase:true,
             trim:true,
         },
-        fullname:{
+        fullName:{
             type:String,
             required:true,
             trim:true,
@@ -39,7 +39,7 @@ const userSchema = new Schema(
             }
         ],
         password:{
-            type:"String",
+            type: String,
             required:[true,"Password is required"]
         },
         refreshToken:{
@@ -66,7 +66,7 @@ userSchema.methods.generateAccessToken = function(){
             _id:this._id,
             email:this.email,
             username:this.username,
-            fullname:this.fullname
+            fullName:this.fullName
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
-const asyncHandler = (requestHandler) => {
-    (req,res,next)=>{
+﻿const asyncHandler = (requestHandler) => {
+    return (req,res,next)=>{
         Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
     }
 }
