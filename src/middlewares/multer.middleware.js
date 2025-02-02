f9da889dcaf02019ed5ec9e4cbebf8d895de6479
Ayush Sahu <sahuayush0613@gmail.com>
2025-02-03 01:15:57 +0530

    add user register controller

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -5,8 +5,9 @@ const storage = multer.diskStorage({
         cb(null,"./public/temp")
     },
     filename:function(req,file,cb){
-        const uniqueSuffix = date.now()+'-'+ Math.round(math.random()*1E9);
-        cb(null,file.fieldname = '-' + uniqueSuffix);//file.originalname can also be used
+        // const uniqueSuffix = date.now()+'-'+ Math.round(math.random()*1E9);
+        // cb(null,file.fieldname = '-' + uniqueSuffix);//file.originalname can also be used
+        cb(null,file.originalname);
     },
 })
 
